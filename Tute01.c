5c571e587d793488204ef37c6ee7f2df67f83cc0
it21345500 <99165022+IT21345500@users.noreply.github.com>
2022-03-09 13:03:42 +0000

    tutoriol 1 is complete

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,22 @@
 #include <stdio.h>
 
 int main() {
-  
+
+
+  float mark1,mark2; 
+    float total, average;
+
+
+  printf("Enter marks of two subjects: \n");
+  scanf("%f%f", &mark1, &mark2);
+
+   
+    total = mark1 + mark2 ;
+    average = total / 2.0;
+    
+  printf("Total marks = %.2f\n", total);
+  printf("Average marks = %.2f\n", average);
+     
   return 0;
 }
 
